d5e0d8c6af0d5f12e593a20c1b29b2efabca95f8
mac <mac@esco-3.local>
2021-08-09 12:17:21 +0100

    persisted data by implementing async storage

--- context/favourites/favourites.context.js
+++ context/favourites/favourites.context.js
@@ -1,9 +1,38 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({children}) => {
   const [favourites, setFavourites] = useState([]);
+
+  const loadFavourites = async () => {
+    try {
+      const fav = await AsyncStorage.getItem('@favourites');
+      if (fav !== null) {
+        setFavourites(JSON.parse(fav));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const saveFavourites = async favs => {
+    try {
+      const jsonVal = JSON.stringify(favs);
+      await AsyncStorage.setItem('@favourites', jsonVal);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   const add = restaurant => {
     setFavourites([restaurant, ...favourites]);
