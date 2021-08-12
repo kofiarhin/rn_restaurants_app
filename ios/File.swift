30ed8770b36f78141e712aed8abfcc66f8da4601
mac <mac@esco-3.local>
2021-08-12 12:34:14 +0100

    added lottie animation to main screen

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  rn_restaurants
+//
+//  Created by mac on 8/12/21.
+//
+
+import Foundation
--- screens/main.screen.js
+++ screens/main.screen.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import {landingBg} from '../constants';
 import {AuthBackground, AuthButton} from '../components/Auth/auth.styles';
 import {SpacerBottom} from '../constants';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export const Main = ({navigation}) => {
   return (
     <AuthBackground>
-      {/* title */}
-      <Text style={styles.title}> Uber Eats </Text>
+      <View
+        style={{
+          width: '100%',
+          height: 150,
+        }}>
+        <LottieView
+          animation="key"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require('../assets/watermelon.json')}
+        />
+      </View>
 
-      <AuthButton
-        icon="lock-open-outline"
-        onPress={() => navigation.navigate('Login')}>
-        {' '}
-        Login
-      </AuthButton>
-      <SpacerBottom />
-      <AuthButton icon="email" onPress={() => navigation.navigate('Register')}>
-        {' '}
-        Register
-      </AuthButton>
+      <View>
+        <Text style={styles.title}> Uber Eats </Text>
+
+        <AuthButton
+          icon="lock-open-outline"
+          onPress={() => navigation.navigate('Login')}>
+          {' '}
+          Login
+        </AuthButton>
+        <SpacerBottom />
+        <AuthButton
+          icon="email"
+          onPress={() => navigation.navigate('Register')}>
+          {' '}
+          Register
+        </AuthButton>
+      </View>
     </AuthBackground>
   );
 };
